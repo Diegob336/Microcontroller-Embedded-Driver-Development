bb3677e01b156faa3fd829072a0078d8812f9c02
Diego Bonilla <diego@MacBookAir.attlocal.net>
2025-07-07 14:20:52 -0400

    Initial commit: added STM32 device header and project setup

---
+++ Course-Projects/stm32f1xx_drivers/Debug/Src/main.d
@@ -0,0 +1,3 @@
+Src/main.o: ../Src/main.c \
+ /Users/diego/Projects/Microcontroller-Embedded-Driver-Development/Course-Projects/stm32f1xx_drivers/drivers/Inc/stm32f103xx.h
+/Users/diego/Projects/Microcontroller-Embedded-Driver-Development/Course-Projects/stm32f1xx_drivers/drivers/Inc/stm32f103xx.h:
---
+++ Course-Projects/stm32f1xx_drivers/drivers/Inc/stm32f103xx_gpio_driver.h
@@ -0,0 +1,13 @@
+/*
+ * stm32f103xx_gpio_driver.h
+ *
+ *  Created on: Jul 7, 2025
+ *      Author: diego
+ */
+
+#ifndef STM32F103XX_GPIO_DRIVER_H_
+#define STM32F103XX_GPIO_DRIVER_H_
+
+
+
+#endif /* STM32F103XX_GPIO_DRIVER_H_ */
